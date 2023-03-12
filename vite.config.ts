a5ecaac6aad178198bd402e001d0c2efd63d1147
fishworm96 <245525860@qq.com>
2023-03-12 23:12:49 +0800

    feat: 添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import autoprefixer from 'autoprefixer'
+import postcssImport from 'postcss-import'
+import tailwindcss from 'tailwindcss'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssImport,
+        autoprefixer,
+        tailwindcss,
+      ],
+    },
+  },
   plugins: [react()],
 })
