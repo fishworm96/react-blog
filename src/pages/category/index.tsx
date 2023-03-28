20ec9099b99dbf9eec155d7b18de27770b8d5fb2
fishworm96 <245525860@qq.com>
2023-03-28 17:02:15 +0800

    style: 添加category样式

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,15 +1,32 @@
 import CardContent from 'components/Card/CardContent'
 import CardHeader from 'components/Card/CardHeader'
 import CardTitle from 'components/Card/CardTitle'
+import { Link } from 'react-router-dom'
 
 const Category = () => {
+  const count = new Array(7).fill(1)
   return (
     <CardHeader>
       <CardTitle>
-        <div className='flex justify-center items-center'>分类</div>
+        <div className='text-5xl'>分类</div>
       </CardTitle>
       <CardContent>
-        <div>123</div>
+        <div className='flex flex-wrap justify-center gap-8'>
+          {
+            count.map(() => (
+              <Link to='/'>
+                <div className='card w-72 h-40 bg-base-100 shadow-xl image-full'>
+                  <figure>
+                    <img src="https://api.lorem.space/image/movie?w=400&h=225" />
+                  </figure>
+                  <div className='card-body'>
+                    <h2 className='card-title'>Movies</h2>
+                  </div>
+                </div>
+              </Link>
+            ))
+          }
+        </div>
       </CardContent>
     </CardHeader>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,11 +10,14 @@ module.exports = {
         '-10': '-10',
       },
     },
-    minHeight: {
-      '3/4': '75vh',
-    },
     fontSize: {
-      '2xl': '1.5rem',
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
   },
   plugins: [require('daisyui')],
